0c74ace29e996698b8223fd718c8a36cb93bdaec
“Ahmad <ahmadjunaidibeds@gmail.com>
2024-10-02 22:46:04 +0700

    Praktikum 03

---
+++ 03_Pengenalan_Dart/GUIDED/main.dart
@@ -0,0 +1,71 @@
+//Variable
+//Variabel dengan var
+// void main() {
+//  // Menggunakan var
+//  var name = "Alice"; // Tipe data String
+//  var age = 25; // Tipe data Integer
+
+//  print("Nama: $name, Usia: $age");
+// }
+
+
+//Switch-Case Statement
+// void main() {
+//  var day = 3; // Misalkan 1 = Senin, 2 = Selasa, dst.
+//  switch (day) {
+//  case 1:
+//  print("Senin");
+//  break;
+//  case 2:
+//  print("Selasa");
+//  break;
+//  case 3:
+//  print("Rabu");
+//  break;
+//  case 4:
+//  print("Kamis");
+//  break;
+//  case 5:
+//  print("Jumat");
+//  break;
+//  case 6:
+//  print("Sabtu");
+//  break;
+//  case 7:
+//  print("Minggu");
+//  break;
+//  default:
+//  print("Hari tidak valid");
+//  }
+// }
+
+//Looping
+// void main() {
+//   // For loop sederhana
+//   for (int i = 1; i <= 5; i++) {
+//     print(i);
+//   }
+// }
+
+// void main() {
+//   int i = 1; // Deklarasi variabel
+//   // While loop sederhana
+//   while (i <= 5) {
+//     print(i);
+//     i++; // Tambahkan 1 ke i setelah setiap iterasi
+//   }
+// }
+
+//condition? true expetion : false expression
+
+//
+
+
+//Menambahkan Parameter
+// void greet(String name, int age) {
+//  print('Hello $name, you are $age years old.');
+// }
+// void main() {
+//  greet('Alice', 25);
+// }
+
