5a5aedfb805642ddde795342b735743caba73c2d
“Ahmad <ahmadjunaidibeds@gmail.com>
2024-10-16 21:55:04 +0700

    update

---
+++ 05_Antarmuka_Pengguna_Lanjutan/GUIDED/praktikum_5/lib/custom_scroll_view.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CustomScrollViewScreen extends StatelessWidget {
+  const CustomScrollViewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Demo"),
+        backgroundColor: Colors.grey[200],
+      ),
+      body: CustomScrollView(
+        slivers: [
+          // Sliver Grid
+          SliverGrid(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.teal[100 * (index % 9)],
+                  child: Text('Grid Item $index'),
+                );
+              },
+              childCount: 20,
+            ),
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+              childAspectRatio: 4,
+            ),
+          ),
+          // Sliver Fixed Extent List
+          SliverFixedExtentList(
+            itemExtent: 50.0,
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.lightBlue[100 * (index % 9)],
+                  child: Text('List Item $index'),
+                );
+              },
+              childCount: 3, // Sesuai dengan contoh gambar
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
